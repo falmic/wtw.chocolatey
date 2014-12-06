668eec0e126ca74c7c84c36506298088d9c90143
falmic <felek@falmic.pl>
2014-12-06 10:53:48 +0100

    v1.1
    Update to new version.
    Add x32 / x64 detect

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,1 +1,26 @@
-Install-ChocolateyPackage 'wtw' 'exe' '/S' 'http://redir.k2t.eu/go/6/'
+$package = 'WTW'
+
+try {
+  $params = @{
+    packageName = $package;
+    fileType = 'msi';
+    silentArgs = '/quiet';
+    url = 'http://redir.k2t.eu/go/40/';
+    url64bit = 'http://redir.k2t.eu/go/41/';
+  }
+
+  Install-ChocolateyPackage @params
+  
+  $IsSytem32Bit = (($Env:PROCESSOR_ARCHITECTURE -eq 'x86') -and `
+    ($Env:PROCESSOR_ARCHITEW6432 -eq $null))
+  if (!$IsSytem32Bit)
+  {
+    $params.url64bit = $params.url
+    Install-ChocolateyPackage @params
+  }
+
+  Write-ChocolateySuccess $package
+} catch {
+  Write-ChocolateyFailure $package "$($_.Exception.Message)"
+  throw
+}
